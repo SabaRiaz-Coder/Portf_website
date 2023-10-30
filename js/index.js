f3674d3bfb54844bf6346adf17656ea0bf027c1f
SabaRiaz-coder <riazsaba85@gmail.com>
2023-10-30 23:37:52 +0500

    design web structue

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// <!-- Setup and start animation! -->
+
+var typed = new Typed('#element', {
+    strings: ['Web developer', 'Graphic Designer', 'Web Desinger', 'SEO Expert'],
+    typeSpeed: 50,
+  });
